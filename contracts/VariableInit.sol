f4edd07be0348171c069d38e901af3e10fbac011
guanms <215216569@qq.com>
2025-05-22 22:22:50 +0800

    第8讲-变量的初始值

---
+++ contracts/VariableInit.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+/**
+ * @title VariableInit
+ * @dev Demonstrates variable initialization in Solidity
+ * 8 变量初始化
+ */
+contract VariableInit {
+
+    uint public x;
+
+    bool public y;
+
+    string public z;
+
+    int public a;
+
+    address public b;
+
+    enum Color {Red, Green, Blue}
+    Color public c;
+
+    function fi() internal {}
+
+    function fe()  external{}
+
+    uint[8] public d;
+    uint[] public e;
+
+    mapping(uint => string) public f;
+}
